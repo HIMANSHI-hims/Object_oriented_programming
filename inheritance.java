0c5127407df7add577f6a7a3ffd7a0a8f0f60805
HIMANSHI-hims <9c.himanshisrivastava@gmail.com>
2026-07-14 21:25:09 +0530

    inheritance

---
+++ inheritance.java
@@ -0,0 +1,116 @@
+class Car{
+    protected String brand;
+    protected String model;
+    protected boolean isEngineOn;
+    protected int  currentSpeed;
+
+    public Car(String brand,String model)
+    {
+        this.brand=brand;
+        this.model=model;
+        isEngineOn=false;
+        currentSpeed=0;
+    }
+    public void startEngine()
+    {
+        isEngineOn=true;
+        System.out.println("engine is started");
+    }
+    public void stopEngine()
+    {
+        isEngineOn=false;
+        currentSpeed=0;
+        System.out.println("engine is turned off");
+    }
+    public void accelerate()
+    {
+        if(!isEngineOn)
+        {
+            System.out.println("first trun on the engine before accelerating");
+            return;
+        }
+        else
+        {
+            currentSpeed+=10;
+            System.out.println("speed is increased by 10km/hr");
+        }
+    }
+    public void brake()
+    {
+        if(!isEngineOn)
+        {
+            System.out.println("the engine is already turned off");
+            return;
+        }
+        else
+        {
+            currentSpeed-=10;
+            if(currentSpeed<0)
+            {
+                currentSpeed=0;
+                System.out.println("brakes are applied");
+            }
+            else
+            {
+                System.out.println("speed is decreasing by 10km/hr");
+            }
+        }
+    }
+}
+class ElectricCar extends Car{
+    public int batteryPower;
+    public ElectricCar(String brand,String model)
+    {
+        super(brand,model);
+        this.batteryPower=100;
+    }
+    public void chargeBattery()
+    {
+        batteryPower=100;
+        System.out.println("battery is fully charged");
+    }
+}
+class ManualCar extends Car{
+    private int currentGear;
+    public ManualCar(String brand,String model)
+    {
+        super(brand,model);
+        currentGear=0;
+    }
+    public void shiftGear(int gear)
+    {
+        if(!isEngineOn)
+        {
+            System.out.println("the engine is not on");
+            return;
+        }
+        else
+        {
+            currentGear=gear;
+            System.out.println("gear is shifted to "+currentGear);
+        }        
+    }
+}
+public class inheritance{
+    public static void main(String[] args)
+    {
+        ElectricCar tesla=new ElectricCar("Tesla","Model S");
+        tesla.startEngine();
+        tesla.accelerate();
+        tesla.brake();
+        tesla.chargeBattery();
+        System.out.println("current speed of the car is "+tesla.currentSpeed+"km/hr");
+        System.out.println("brand of the car is "+tesla.brand);
+        System.out.println("model of the car is "+tesla.model);
+        System.out.println("battery power of the car is "+tesla.batteryPower+"%");
+        
+        ManualCar honda=new ManualCar("Honda","Civic");
+        honda.startEngine();
+        honda.shiftGear(2);
+        honda.accelerate();
+        honda.brake();
+        System.out.println("current speed of the car is "+honda.currentSpeed+"km/hr");
+        System.out.println("brand of the car is "+honda.brand);
+        System.out.println("model of the car is "+honda.model);
+    }
+}
